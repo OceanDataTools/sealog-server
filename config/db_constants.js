41a78b8d826bf1a1c5578c4bcc74933c37742d06
Webb Pinner <webbpinner@gmail.com>
2017-07-16 10:06:58 -0400

    Import of db_constants.js file

---
+++ config/db_constants.js
@@ -0,0 +1,19 @@
+const eventloggerDB = 'eventlogger';
+const eventloggerDB_devel = 'eventlogger_devel';
+const usersTable = 'users';
+const eventsTable = 'events';
+const auxDataTable = 'event_aux_data';
+const eventDefinitionTable = 'event_definitions';
+const eventTemplateTable = 'event_templates';
+const eventExportTemplateTable = 'event_export_templates';
+
+module.exports = {
+  eventloggerDB,
+  eventloggerDB_devel,
+  usersTable,
+  eventsTable,
+  auxDataTable,
+  eventDefinitionTable,
+  eventTemplateTable,
+  eventExportTemplateTable
+}
