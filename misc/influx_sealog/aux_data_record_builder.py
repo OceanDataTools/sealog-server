e2e801c4f820e0acbeadecb766aa5f143f5e2ce3
ljones <ljones@oet.org>
2025-11-25 10:42:50 -0800

    Make AuxDataRecordBuilder base class and inherit from it in the Influx and Coriolix implementations
    Note: initial refactor was done by CoPilot and then edited by me

--- misc/influx_sealog/aux_data_record_builder.py
+++ misc/influx_sealog/aux_data_record_builder.py
@@ -17,7 +17,6 @@ LICENSE INFO:   This code is licensed under MIT license (see LICENSE.txt for det
                 Copyright (C) OceanDataTools.org 2025
 '''
 import sys
-import json
 import logging
 from datetime import datetime, timedelta
 from urllib3.exceptions import NewConnectionError
@@ -26,6 +25,7 @@ from influxdb_client.rest import ApiException
 from os.path import dirname, realpath
 sys.path.append(dirname(dirname(dirname(realpath(__file__)))))
 
+from misc.base_aux_data_record_builder import AuxDataRecordBuilder
 from misc.influx_sealog.settings import (
     INFLUXDB_URL,
     INFLUXDB_AUTH_TOKEN,
@@ -34,23 +34,19 @@ from misc.influx_sealog.settings import (
 )
 
 
-class SealogInfluxAuxDataRecordBuilder():
+class SealogInfluxAuxDataRecordBuilder(AuxDataRecordBuilder):
     '''
     Class that handles the construction of an influxDB query and using the
     resulting data to build a sealog aux_data record.
     '''
 
     def __init__(self, influxdb_client, aux_data_config, influxdb_bucket=INFLUXDB_BUCKET):
+        super().__init__(aux_data_config)
         self._influxdb_client = influxdb_client.query_api()
         self._influxdb_bucket = (
             aux_data_config['query_bucket']
             if 'query_bucket' in aux_data_config else influxdb_bucket
         )
-        self._query_measurements = aux_data_config['query_measurements']
-        self._query_fields = list(aux_data_config['aux_record_lookup'].keys())
-        self._aux_record_lookup = aux_data_config['aux_record_lookup']
-        self._data_source = aux_data_config['data_source']
-        self.logger = logging.getLogger(__name__)
 
     @staticmethod
     def _build_query_range(ts):
@@ -94,120 +90,6 @@ class SealogInfluxAuxDataRecordBuilder():
         logging.debug("Query: %s", query)
         return query
 
-    def _build_aux_data_dict(
-        self, event_id, influx_query_result
-    ):  # pylint:disable=R0915
-        '''
-        Internal method to build the sealog aux_data record using the event_id,
-        influx_query_result and the class instance's datasource value.
-        '''
-
-        aux_data_record = {
-            'event_id': event_id,
-            'data_source': self._data_source,
-            'data_array': []
-        }
-
-        influx_data = {
-        }
-
-        for table in influx_query_result:
-            for record in table.records:
-
-                influx_data[record.get_field()] = record.get_value()
-
-        logging.debug("raw values: %s", json.dumps(influx_data, indent=2))
-
-        if not influx_data:
-            return None
-
-        for key, value in self._aux_record_lookup.items():
-            try:
-                if "no_output" in value and value['no_output'] is True:
-                    continue
-
-                if key not in influx_data:
-                    continue
-
-                output_value = influx_data[key]
-
-                if "modify" in value:
-                    logging.debug("modify found in record")
-                    for mod_op in value['modify']:
-                        test_result = True
-
-                        if 'test' in mod_op:
-                            logging.debug("test found in mod_op")
-                            test_result = False
-
-                            for test in mod_op['test']:
-                                logging.debug(json.dumps(test))
-
-                                if 'field' in test:
-
-                                    if test['field'] not in influx_data:
-                                        logging.error("test field data not in influx query")
-                                        return None
-
-                                    if 'eq' in test and influx_data[test['field']] == test['eq']:
-                                        test_result = True
-                                        break
-
-                                    if 'gt' in test and influx_data[test['field']] > test['gt']:
-                                        test_result = True
-                                        break
-
-                                    if 'gte' in test and influx_data[test['field']] >= test['gt']:
-                                        test_result = True
-                                        break
-
-                                    if 'lt' in test and influx_data[test['field']] < test['lt']:
-                                        test_result = True
-                                        break
-
-                                    if 'lte' in test and influx_data[test['field']] <= test['lt']:
-                                        test_result = True
-                                        break
-
-                                    if 'ne' in test and influx_data[test['field']] != test['ne']:
-                                        test_result = True
-                                        break
-
-                        if test_result and 'operation' in mod_op:
-                            logging.debug("operation found in mod_op")
-                            for operan in mod_op['operation']:
-
-                                if 'add' in operan:
-                                    output_value += operan['add']
-
-                                if 'subtract' in operan:
-                                    output_value -= operan['subtract']
-
-                                if 'multiply' in operan:
-                                    output_value *= operan['multiply']
-
-                                if 'divide' in operan:
-                                    output_value /= operan['divide']
-
-                aux_data_record['data_array'].append({
-                    'data_name': value['name'],
-                    'data_value': (
-                        str(round(output_value, value['round']))
-                        if 'round' in value
-                        else str(output_value)
-                    ),
-                    'data_uom': value['uom'] if 'uom' in value else ''
-                })
-            except ValueError as exc:
-                logging.warning("Problem adding %s", key)
-                logging.debug(str(exc))
-                continue
-
-        if len(aux_data_record['data_array']) > 0:
-            return aux_data_record
-
-        return None
-
     def build_aux_data_record(self, event):
         '''
         Build the aux_data record for the given event.
@@ -239,36 +121,14 @@ class SealogInfluxAuxDataRecordBuilder():
                 logging.error(str(exc))
                 raise exc
         else:
-            aux_data_record = self._build_aux_data_dict(event['id'], query_result)
+            # Parse InfluxDB result into a dictionary format
+            influx_data = {}
+            for table in query_result:
+                for record in table.records:
+                    influx_data[record.get_field()] = record.get_value()
+
+            aux_data_record = self._build_aux_data_dict(event['id'], influx_data)
 
             return aux_data_record
 
         return None
-
-    @property
-    def data_source(self):
-        '''
-        Getter method for the data_source property
-        '''
-        return self._data_source
-
-    @property
-    def measurements(self):
-        '''
-        Getter method for the _query_measurements property
-        '''
-        return self._query_measurements
-
-    @property
-    def fields(self):
-        '''
-        Getter method for the _query_fields property
-        '''
-        return self._query_fields
-
-    @property
-    def record_lookup(self):
-        '''
-        Getter method for the _aux_record_lookup property
-        '''
-        return self._aux_record_lookup
